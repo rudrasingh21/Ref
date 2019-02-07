6012eadfdfc3defd23c3a2d217091fe502549a0d
rudrasingh21 <30667041+rudrasingh21@users.noreply.github.com>
2019-02-06 21:52:25 -0800

    Add files via upload

--- inExecutorEntryLogLevelAll.py
+++ inExecutorEntryLogLevelAll.py
@@ -1,7 +1,6 @@
-"""Used for demonstrating log level
+"""Shows how to set log level and run a function that writes in executor
 
-Execute with
-  spark2-submit inExecutorLogLevel.py
+Execute with spark2-submit inExecutorLogLevelAll.py
 Demo for Pluralsight course:
   Developing Spark Applications Using Python and Cloudera
 Created by Xavier Morera
@@ -15,11 +14,12 @@ def inExecutor(entry):
   return "printed: " + str(entry[0])
 
 def main():
-    sc = SparkContext()
+    sc = SparkContext().getOrCreate()
     sc.setLogLevel("ALL")
 
     test_write = sc.parallelize(['xavier', 'troy'])
     test_write.map(lambda x: inExecutor(x)).collect()
+    sc.setLogLevel("WARN")
 
 if __name__ == "__main__":
     main()
